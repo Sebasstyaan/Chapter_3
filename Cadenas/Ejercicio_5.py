7982a109894b4e563ac0ce3b013f8ba92b667b67
Sebasstyaan <scvargas40@misena.edu.co>
2022-03-20 16:54:31 -0500

    ejercicio
    ejercicio

--- Cadenas/Ejercicio_5.py
+++ Cadenas/Ejercicio_5.py
@@ -1,4 +1,13 @@
 #Escriba un programa en Python que acepte una palabra en castellano y calcule una
 #métrica que sea el número total de caracteres de la palabra multiplicado por el número
 #total de vocales que contiene la palabra
-palabra=(input("Digite palabra: "))
+texto=(input("Digite palabra: "))
+convercion=len(texto)
+a=texto.count('a')
+e=texto.count('e')
+i=texto.count('i')
+o=texto.count('o')
+u=texto.count('u')
+metrica=convercion*(a+e+i+o+u)
+print(f'Resultado: {metrica}')
+print(f'{convercion}')
