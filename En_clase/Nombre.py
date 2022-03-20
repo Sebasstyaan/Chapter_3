689aabd0e10d04eb42e32dcf24791daf7c33e029
Sebasstyaan <scvargas40@misena.edu.co>
2022-03-20 17:53:12 -0500

    Ejercicios
    Ejercicios

---
+++ En_clase/Nombre.py
@@ -0,0 +1,4 @@
+nombre=input("Digite nombre: ")
+edad=input("Digite la edad: ")
+#print('mi nombre es'), print(nombre), print('y tengo'), print(edad), print("años")
+print(f"Mi nombre es: {nombre} y tengo: {edad} años")
