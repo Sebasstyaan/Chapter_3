30f83a0389ef5ab3672677f53c22eb5612e4622b
Sebasstyaan <scvargas40@misena.edu.co>
2022-03-20 11:20:36 -0500

    Ejercicios
    Ejercicios

--- Ejercicios_de_repaso/Ejercicio_1.py
+++ Ejercicios_de_repaso/Ejercicio_1.py
@@ -1,1 +1,6 @@
-print(oct(3235))
+#Escriba un programa en Python que acepte el radio de un círculo y compute su área
+#Entrada: 5 Salida: 78.5
+r=int(input("Digite el valor del radio:  "))
+pi=3.1416
+A=pi*r**2
+print(A)
